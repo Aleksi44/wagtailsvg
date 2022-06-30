6af275a16a42ba5819e4373d255ab13a35c6b9da
Alexander Bückig <AlexBueckig@gmail.com>
2022-06-30 19:32:36 +0200

    add fallback implementation for wagtail 2

--- wagtailsvg/edit_handlers.py
+++ wagtailsvg/edit_handlers.py
@@ -1,12 +1,23 @@
 try:
-    from wagtail.admin.panels import BaseChooserPanel
+    from wagtail.admin.panels import FieldPanel
+    from wagtailsvg.widgets import AdminSvgChooser
+
+
+    class SvgChooserPanel(FieldPanel):
+        def __init__(self, field_name, disable_comments=None, permission=None, **kwargs):
+            super().__init__(field_name, **kwargs)
+            self.widget = AdminSvgChooser
+            self.disable_comments = disable_comments
+            self.permission = permission
+
 except ImportError:
     from wagtail.admin.edit_handlers import BaseChooserPanel
 
 
-class SvgChooserPanel(BaseChooserPanel):
-    object_type_name = "svg"
+    class SvgChooserPanel(BaseChooserPanel):
+        object_type_name = "svg"
 
-    def widget_overrides(self):
-        from wagtailsvg.widgets import AdminSvgChooser
-        return {self.field_name: AdminSvgChooser}
+        def widget_overrides(self):
+            from wagtailsvg.widgets import AdminSvgChooser
+            return {self.field_name: AdminSvgChooser}
+
