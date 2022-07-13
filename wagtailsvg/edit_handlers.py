dc9f14df4030da68e217008dcb0afd46270b5acd
Alexis Le Baron <hello@snoweb.fr>
2022-07-13 06:58:20 +0200

    fix flake

--- wagtailsvg/edit_handlers.py
+++ wagtailsvg/edit_handlers.py
@@ -2,7 +2,6 @@ try:
     from wagtail.admin.panels import FieldPanel
     from wagtailsvg.widgets import AdminSvgChooser
 
-
     class SvgChooserPanel(FieldPanel):
         def __init__(self, field_name, disable_comments=None, permission=None, **kwargs):
             super().__init__(field_name, **kwargs)
@@ -13,11 +12,9 @@ try:
 except ImportError:
     from wagtail.admin.edit_handlers import BaseChooserPanel
 
-
     class SvgChooserPanel(BaseChooserPanel):
         object_type_name = "svg"
 
         def widget_overrides(self):
             from wagtailsvg.widgets import AdminSvgChooser
             return {self.field_name: AdminSvgChooser}
-
